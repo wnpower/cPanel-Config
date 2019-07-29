c68e711c8616ef9179d369cef923f68658e49ff8
imorandinwnp <imorandin@wnpower.com>
2019-07-29 10:49:53 -0300

    disable mailman

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -269,6 +269,7 @@ whmapi1 set_tweaksetting key=email_outbound_spam_detect_action value=block
 whmapi1 set_tweaksetting key=email_outbound_spam_detect_enable value=1
 whmapi1 set_tweaksetting key=email_outbound_spam_detect_threshold value=120
 whmapi1 set_tweaksetting key=skipspambox value=0
+whmapi1 set_tweaksetting key=skipmailman value=1
 
 sed -i 's/^phpopenbasedirhome=.*/phpopenbasedirhome=1/' /var/cpanel/cpanel.config
 sed -i 's/^minpwstrength=.*/minpwstrength=70/' /var/cpanel/cpanel.config
