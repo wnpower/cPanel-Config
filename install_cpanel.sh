53e2765c3a6dfb319bd2e27c46465c6720566da5
imorandinwnp <imorandin@wnpower.com>
2019-11-19 11:14:47 -0300

    undo emiticons

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -558,14 +558,6 @@ done
 /scripts/restartsrv_httpd
 /scripts/restartsrv_apache_php_fpm
 
-# EMOTICONES ROUNDCUBE
-if [ -d /usr/local/cpanel/base/3rdparty/roundcube/plugins/emoticons/ ]; then
-        echo "Activando emoticons en Roundcube..."
-        cp -af /usr/local/cpanel/base/3rdparty/roundcube/config/config.inc.php /usr/local/cpanel/base/3rdparty/roundcube/config/config.inc.php.bak
-        sed -i "s/\$config\['plugins'\] \(.*\));/\$config\['plugins'\] \1,'emoticons');/" /usr/local/cpanel/base/3rdparty/roundcube/config/config.inc.php
-        echo "Listo!"
-fi
-
 echo "Limpiando...."
 
 history -c
