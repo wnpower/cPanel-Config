a930cc55468a2cccdd36bf2d7e97b6cde12462a9
imorandinwnp <imorandin@wnpower.com>
2020-01-16 12:22:40 -0300

    disable MAXMIND false positives

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -175,14 +175,14 @@ sed -i '/^#SPAMDROP/s/^#//' /etc/csf/csf.blocklists
 sed -i '/^#SPAMEDROP/s/^#//' /etc/csf/csf.blocklists
 sed -i '/^#DSHIELD/s/^#//' /etc/csf/csf.blocklists
 sed -i '/^#HONEYPOT/s/^#//' /etc/csf/csf.blocklists
-sed -i '/^#MAXMIND/s/^#//' /etc/csf/csf.blocklists
+#sed -i '/^#MAXMIND/s/^#//' /etc/csf/csf.blocklists FALSOS POSITIVOS
 sed -i '/^#BDE|/s/^#//' /etc/csf/csf.blocklists
 
 sed -i '/^SPAMDROP/s/|0|/|300|/' /etc/csf/csf.blocklists
 sed -i '/^SPAMEDROP/s/|0|/|300|/' /etc/csf/csf.blocklists
 sed -i '/^DSHIELD/s/|0|/|300|/' /etc/csf/csf.blocklists
 sed -i '/^HONEYPOT/s/|0|/|300|/' /etc/csf/csf.blocklists
-sed -i '/^MAXMIND/s/|0|/|300|/' /etc/csf/csf.blocklists
+#sed -i '/^MAXMIND/s/|0|/|300|/' /etc/csf/csf.blocklists # FALSOS POSITIVOS
 sed -i '/^BDE|/s/|0|/|300|/' /etc/csf/csf.blocklists
 
 sed -i '/^TOR/s/^TOR/#TOR/' /etc/csf/csf.blocklists
