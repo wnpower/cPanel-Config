c31bd3fa6729dc72b4626183ca57b0096d804183
imorandinwnp <imorandin@wnpower.com>
2020-10-23 15:17:25 -0300

    se actualiza a mariadb 10.3

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -482,8 +482,8 @@ sed  -i '/\[mysqld\]/a # WNPower pre-configured values' /etc/my.cnf
 
 /scripts/restartsrv_mysql
 
-echo "Actualizando a MariaDB 10.2..."
-whmapi1 start_background_mysql_upgrade version=10.2
+echo "Actualizando a MariaDB 10.3..."
+whmapi1 start_background_mysql_upgrade version=10.3
 
 echo "Configurando feature disabled..."
 whmapi1 update_featurelist featurelist=disabled api_shell=0 agora=0 analog=0 boxtrapper=0 traceaddy=0 modules-php-pear=0 modules-perl=0 modules-ruby=0 pgp=0 phppgadmin=0 postgres=0 ror=0 serverstatus=0 webalizer=0 clamavconnector_scan=0 lists=0
