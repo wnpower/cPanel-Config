2b76169483a6bdd2a070f7f301a4743340ad8ef6
Ignacio Morandin <imorandin@wnpower.com>
2023-05-02 14:41:59 -0300

    mariadb 10.6 by default

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -76,9 +76,9 @@ if [ -f /usr/local/cpanel/cpanel ]; then
 else
 	hostname -f > /root/hostname
 
-	# INSTALAR MARIADB 10.5 POR DEFECTO https://cloudlinux.zendesk.com/hc/en-us/articles/360020599839
+	# INSTALAR MARIADB 10.6 POR DEFECTO https://cloudlinux.zendesk.com/hc/en-us/articles/360020599839
 	mkdir -p /root/cpanel_profile/
-	echo "mysql-version=10.5" >> /root/cpanel_profile/cpanel.config
+	echo "mysql-version=10.6" >> /root/cpanel_profile/cpanel.config
 
 	cd /home && curl -o latest -L https://securedownloads.cpanel.net/latest && sh latest --skip-cloudlinux
 
